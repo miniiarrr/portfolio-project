490750c58dc45e7df63ef4939bbfd68367027b1b
Miniiar Rakhimkulov <miniyar23@gmail.com>
2024-01-14 12:40:55 +0100

    NavigationBar

--- src/screens/navigationBar/NavigationBar.js
+++ src/screens/navigationBar/NavigationBar.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { navigationLinks } from '../../helpers/navigationLinks';
+
+function renderLinks() {
+  return navigationLinks.map((e, idx) => (
+    <Nav.Item id={idx}>
+      <Nav.Link href={e.ref}>{e.name}</Nav.Link>
+    </Nav.Item>
+  ));
+}
 
 function NavigationBar() {
-  return <div>NavigationBar</div>;
+  return (
+    <div>
+      <Navbar expand="md" className="bg-primary">
+        <Navbar.Brand>Rakhimkulov Miniiar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav>{renderLinks()}</Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
 export default NavigationBar;
